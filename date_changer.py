48852ec91fb1bc81a4c09b68220082b044ce1b4b
Елена <orderkgpremium@gmail.com>
2024-12-31 19:00:03 +0200

    HW-date_changer

--- date_changer.py
+++ date_changer.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
-created_date = datetime.now().strftime('26-12-2024')
-issue_date = datetime.now().strftime('31-12-2024')
+created_date = '26.12.2024'
+issue_date = '31.12.2024'
 
 print('Дата создания заметки:', created_date[0:5])
 print('Дата истечения заметки (дэдлайн):', issue_date[0:5])
+
+
+
