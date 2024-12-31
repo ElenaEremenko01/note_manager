54354fa9a1b91b58ddd8c28791dc710f646964a4
Елена <orderkgpremium@gmail.com>
2024-12-31 19:00:52 +0200

    HW-greetings

--- greetings.py
+++ greetings.py
@@ -2,12 +2,8 @@ username = 'Elena'
 title = 'Homework'
 content = 'To create 6 covariates'
 status = 'In work'
-# импортируем из библиотеки datetime класс date, чтоб работать с датами
-from datetime import datetime
-#используем функцию now, чтоб вывести формат даты, а затем strftime, чтоб вывести дату в
-#                                                                    определенном формате
-created_date = datetime.now().strftime('27-12-2024')
-issue_date = datetime.now().strftime('31-12-2024')
+created_date = '26.12.2024'
+issue_date = '31.12.2024'
 
 print('Имя пользователя:', username)
 print('Заголовок заметки:', title)
