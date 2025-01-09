700a093481dd147166115fefc6b0310af5c48e58
Елена <orderkgpremium@gmail.com>
2025-01-09 22:17:25 +0200

    HW-add_titles_loop

--- delete_note.py
+++ delete_note.py
@@ -2,15 +2,22 @@
 note_try = [{'Имя': 'Алексей', 'Заголовок': 'Купить продукты на неделю', 'Описание': 'Купить продукт на неделю'},
          {'Имя': 'Мария', 'Заголовок': 'Учеба', 'Описание': 'Подготовиться к экзамену'},
          {'Имя': 'Елена', 'Заголовок': 'Книга', 'Описание': 'Написать книгу'}]
+# создаем функцию для отображения списка заметок
+def display_notes(notes):
+    if not notes:
+        print('Список заметок пуст')
+    else:
+        print('Текущий список заметок:')
+        for i in range(len(notes)):
+            print(f'{i+1}. Имя: {notes[i]['Имя']}')
+            print(f'Заголовок: {notes[i]['Заголовок']}')
+            print(f'Описание: {notes[i]['Описание']}')
 
-print('Текущий список заметок:')
-for i in range(len(note_try)):
-    print(f'{i+1}. Имя: {note_try[i]['Имя']}')
-    print(f'Заголовок: {note_try[i]['Заголовок']}')
-    print(f'Описание: {note_try[i]['Описание']}')
-deletion = input('Желаете удалить заметку?(да\нет)').lower()
+display_notes(note_try)
+# запускаем цикл, с запросом удаления заметки
 while True:
-    if deletion == 'да':
+    deletion = input('Желаете удалить заметку?(да\нет)').lower()
+    if deletion == 'да': # Если заметку хотят удалить запускаем цикл для проверки введенных параметров
         note_del = input('Введите имя пользователя или заголовок для удаления заметки:').lower()
         for i in reversed(range(len(note_try))):
             if note_try[i]['Имя'].lower() == note_del:
@@ -18,49 +25,27 @@ while True:
                 if confirmation2 == 'да':
                     del note_try[i]
                     print('Заметка успешно удалена')
-                    for j in reversed(range(len(note_try))):
-                        print('Текущий список заметок:')
-                        print(f'{j + 1}. Имя: {note_try[j]['Имя']}')
-                        print(f'Заголовок: {note_try[j]['Заголовок']}')
-                        print(f'Описание: {note_try[j]['Описание']}')
-                        break
+                    display_notes(note_try)
+                    break
             elif note_try[i]['Заголовок'].lower() == note_del:
                 confirmation3 = input('Вы уверены, что хотите удалить заметку?(да\нет)').lower()
                 if confirmation3 == 'да':
                     del note_try[i]
                 print('Заметка успешно удалена')
-                for j in reversed(range(len(note_try))):
-                    print('Текущий список заметок:')
-                    print(f'{j + 1}. Имя: {note_try[j]['Имя']}')
-                    print(f'Заголовок: {note_try[j]['Заголовок']}')
-                    print(f'Описание: {note_try[j]['Описание']}')
-                    break
-            elif note_try[i]['Имя'].lower() != note_del:
-                print('Заметки с данным именем нет')
+                display_notes(note_try)
                 break
-            elif note_try[i]['Заголовок'].lower() != note_del:
-                print('Заметки с данным заголовком нет')
-                break
-            elif i not in note_try:
-                print('Список заметок пуст')
-                break
-    elif not note_try:
-            print('Список заметок пуст')
-            break
-    elif deletion == 'нет':
-        print('Текущий список заметок:')
-        for i in range(len(note_try)):
-            print(f'{i + 1}. Имя: {note_try[i]['Имя']}')
-            print(f'Заголовок: {note_try[i]['Заголовок']}')
-            print(f'Описание: {note_try[i]['Описание']}')
+            else:
+                print('Заметки с такими параметрами нет')
+                display_notes(note_try)
+    elif deletion == 'нет': # Если заметку не хотят удалять программа завершиться
+        display_notes(note_try)
         break
-    else:
+    elif not note_try: # Если заметок не останется на экране отобразиться следующий ответ
+        print('Список заметок пуст')
+        break
+    else: # Если ввод будет неверным цикл повториться и отобразится подсказка
         print('Ошибка неверный ввод. Ответ может быть: "да" или "нет"')
-        print('Текущий список заметок:')
-        for i in range(len(note_try)):
-            print(f'{i + 1}. Имя: {note_try[i]['Имя']}')
-            print(f'Заголовок: {note_try[i]['Заголовок']}')
-            print(f'Описание: {note_try[i]['Описание']}')
-        deletion = input('Желаете удалить заметку?(да\нет)').lower()
+        display_notes(note_try)
+
 
 
